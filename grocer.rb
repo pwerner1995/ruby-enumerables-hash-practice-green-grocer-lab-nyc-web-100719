a38164834fb83c84bfb0474a56200301f6dceef3
Peter Werner <pcw1995@yahoo.com>
2019-09-11 22:15:28 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -9,16 +9,14 @@ def consolidate_cart(cart)
     cart_hash = cart_hash.merge(cart[i])
     i += 1 
   end
-  p cart_hash
+  cart_hash
 end
 
 def apply_coupons(cart, coupons)
   i = 0 
   coupon_hash = {}
-  p coupons 
   while coupons.length> i do 
     cart.each_key do |key|
-        
       if coupons[i][:item] == key && !coupon_hash.key?("#{key} W/COUPON")
         coupon_hash["#{key} W/COUPON"]={
             :price => (coupons[i][:cost]/coupons[i][:num]),
@@ -26,22 +24,38 @@ def apply_coupons(cart, coupons)
             :count => coupons[i][:num]
         }
         cart[key][:count] -= coupons[i][:num]
-      elsif coupons[i][:item] == key && coupon_hash.key?("#{key} W/COUPON")
+      elsif coupons[i][:item] == key && coupon_hash.key?("#{key} W/COUPON") && cart[key][:count] > coupon_hash["#{key} W/COUPON"][:count]
         coupon_hash["#{key} W/COUPON"][:count] = (coupons[i][:num] * coupons.count(coupons[i]))
         cart[key][:count] -= coupons[i][:num]
       end
     end
     i +=1 
   end
-  
   cart = cart.merge(coupon_hash)
-  p cart 
+  cart 
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each_key do |key|
+    if cart[key][:clearance] == true 
+      cart[key][:price] = (cart[key][:price] * 0.80).round(2)
+    end
+  end
+  cart 
 end
 
 def checkout(cart, coupons)
-  # code here
+  cons_cart = {}
+  total = 0 
+  cons_cart = consolidate_cart(cart)
+  cons_cart = apply_coupons(cons_cart,coupons)
+  cons_cart = apply_clearance(cons_cart)
+  cons_cart.each_key do |key|
+    total += (cons_cart[key][:price] * cons_cart[key][:count])
+  end
+  if total>100 
+    total = (total *0.90).round(2)
+  end 
+  p cons_cart
+  p total 
 end
