c93e55ab8b7c932b50ac45d3791a63fe55e97141
Peter Werner <pcw1995@yahoo.com>
2019-09-11 22:02:10 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -47,7 +47,8 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  cons_cart = cart.consolidate_cart
+  cons_cart = {}
+  cons_cart = consolidate_cart(cart)
   cons_cart = apply_coupons(cons_cart)
   cons_cart = apply_clearance(cons_cart)
   p cons_cart
